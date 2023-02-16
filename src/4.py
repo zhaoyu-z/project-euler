fbf704ed275409bb90b9ea7dd5e984eb4d8fa3af
Zhayu517 <zhangzy517@gmail.com>
2023-02-16 17:47:31 +0000

    clean up q4

--- src/4.py
+++ src/4.py
@@ -4,8 +4,8 @@ def is_palindrome(n):
 largest_palindrome = 0
 factors = (0, 0)
 
-for i in range(100, 1000):
-    for j in range(100, 1000):
+for i in range(900, 1000):
+    for j in range(900, 1000):
         product = i * j
         if is_palindrome(product) and product > largest_palindrome:
             largest_palindrome = product
