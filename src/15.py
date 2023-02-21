26f685e2dff84100d0eb7557931f048def6a831f
Zhayu517 <zhangzy517@gmail.com>
2023-02-21 23:27:32 +0000

    solved q15

---
+++ src/15.py
@@ -0,0 +1,13 @@
+def number_of_routes_from_top_left_to_bottom_right(n):
+    dp = [[0] * (n+1) for _ in range(n+1)]
+    dp[0][0] = 1
+    for i in range(1, n+1):
+        dp[i][0] = 1
+        dp[0][i] = 1
+    
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    return dp[-1][-1]
+
+print(number_of_routes_from_top_left_to_bottom_right(20))
