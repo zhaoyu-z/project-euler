b8c223a484e7f40347c362108ee304fe70c07c1f
Zhayu517 <zhangzy517@gmail.com>
2023-03-16 00:02:34 +0000

    clean up q47 code

--- src/47.py
+++ src/47.py
@@ -1,34 +1,3 @@
-from sympy import factorint, isprime
-
-# def four_are_distinct_primes(num):
-#     a, b, c, d = num, num+1, num+2, num+3
-#     # if not isprime(b) or not isprime(c) or not isprime(d):
-#     #     return False
-#     a1 = factorint(a).keys()
-#     b1 = factorint(b).keys()
-#     c1 = factorint(c).keys()
-#     d1 = factorint(d).keys()
-#     print(a1)
-#     print(b1)
-#     print(c1)
-#     print(d1)
-#     if a1 != b1 and b1 != c1 and c1 != d1 and a1 != d1:
-#         return True
-#     return False
-
-# # print(four_are_distinct_primes(134043))
-
-# i = 647
-# while True:
-#     if four_are_distinct_primes(i):
-#         print(i)
-#         break
-#     i += 2
-
-
-
-
-
 def npf(number):
     """function which will return
     the number of prime factors"""
@@ -42,19 +11,18 @@ def npf(number):
         i += 1
     return (len(a)+1)
 
-# iterator
-j = 2*3*5*7
+def four_are_distinct_primes():
+    j = 2*3*5*7
 
+    while True:
+        if npf(j) == 4:
+            j += 1
+            if npf(j) == 4:
+                j += 1
+                if npf(j) == 4:
+                    j += 1
+                    if npf(j) == 4:
+                        return j-3
+        j += 1
 
-# while True:
-#     if npf(j) == 4:
-#         j += 1
-#         if npf(j) == 4:
-#             j += 1
-#             if npf(j) == 4:
-#                 j += 1
-#                 if npf(j) == 4:
-#                     print(j-3)
-#                     break
-#     j += 1
-
+print(four_are_distinct_primes())
